bd336f6c454430440d98d3be41c51f428fd9d8c0
xiaoquan_0425 <16437211+li0425-xiaoquan@user.noreply.gitee.com>
2026-04-28 21:26:16 +0800

    rename robot asset and fix path import

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { resolve } from 'path'
 // https://vite.dev/config/
 
 export default defineConfig({
